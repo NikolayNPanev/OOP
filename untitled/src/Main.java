51016b63a7bbe4069acfeca313c96063dbb49789
Nikolay Panev <nikolay.n.panev@gmail.com>
2024-03-28 09:20:43 +0200

    Basic Frame

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,16 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /////////////////////////
+        //  CREATE COMPONENTS  //
+        /////////////////////////
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        JFrame f = new JFrame();
+        JButton b = new JButton("click");
+        b.setBounds(100,100,100,50);
 
-        }
+
+
+        ///////////////////////
+        //  COMPONENT LOGIC  //
+        ///////////////////////
+
+
+        //////////////////////////////
+        //  ADD COMPONENTS TO FRAME //
+        //////////////////////////////
+        f.add(b);
+
+
+        ////////////////////
+        //  FRAME LAYOUT  //
+        ////////////////////
+        f.setSize(600,400);
+        f.setLayout(null);
+        f.setVisible(true);
+
+
     }
 }
